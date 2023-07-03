2a37dc7bafc80af987fd8c32584b8d5be865f94d
aashishChakra <ashishchakradhar00@gmail.com>
2023-07-03 15:36:02 +0545

    hawa vo

--- README.txt
+++ README.txt
@@ -4,8 +4,19 @@ The login header file conist of :-
 - class consist of following functions:
 1. void signin()
     //it is used to read user id and password from the user 
-2. bool verifySignin(string , string)
-    //bool data type returns two values which are true and false
-    // it is generally called by signin() function
-    //it as two passing arguments: userId and password respectively, where both are of string data type
-    //this function is called to verify if the the entered user name and password are valid or not
+2. void signup()
+    //it is used to create new accout in the system
+    //the created id can either be for admin or for author
+    //the power selection is done by user himself
+    //the user id is auto generated
+    //the begining password is default and system provided
+    //it consist of validation for all input details
+3. void resetPassword()
+    //it is option for forgot password
+    //it resets the password to default password
+    //user need to input his first name, user id and phone number for conformation
+4. void changePassword()
+    //it is used to change the password of user
+    //it first asks for old password 
+    //it then asks new password 
+    //it then asks to confirm new password
--- login.cpp
+++ login.cpp
@@ -8,47 +8,86 @@
 using namespace std;
 class login{
     protected:
+    
         char ch;
         string userId, password, fname, lname, address, phone, email, power;
     public:
+        void getpassword(char* choice){
+            string newPs,cfmPs;
+            if(choice == "reset"){
+                password = "reset";
+            }
+            else if(choice == "change"){
+                do{
+                    cout<<"Enter New Password: ";
+                    cin>>newPs;
+                    cout<<"Confirm Password: ";
+                    cin>>cfmPs;
+                    if(newPs == cfmPs){
+                        password = cfmPs;
+                    }
+                    else{
+                        cout<<"password did not match";
+                    }
+                }while(newPs != cfmPs);
+            }
+        }
+        string validEmail();
+        void writetofile();
+        void resetPassword(char* , char*);
+        void changePassword(char* , char*);
         void signin();
         void signup();
-        void resetPassword();
         void display();
-}l;
+};
 int main(){
     system("cls");
     login l,m;
     fstream fin,fout;
+    char id[10], ph[10], ps[25];
     int choice=1;
     cout<<"Welcome to TMS"<<endl;
-    while(choice != 0){
+    while(choice != 10){
         system("cls");
         cout<<"1. Login"<<endl
         <<"2. Register"<<endl
         <<"3. Forgot Password"<<endl
+        <<"4. Change Passwotd"<<endl
+        <<"5. Display"<<endl
         <<"Enter your Choice:";
         cin>>choice;
         switch (choice){
-            case 0:
-                choice = 0;
+            case 0://nothin
+                choice = 10;
                 break;
-            case 1:
+            case 1://login to system
                 l.signin();
                 break;
-            case 2:
-                fout.open("login.txt",ios::out | ios:: app);
+            case 2://register t system
                 l.signup();
-                fout.write((char*)&l, sizeof(l));
-                fout.close();
+                l.writetofile();
                 break;
-            case 3:
-                fin.open("login.txt",ios::in);
+            case 3://reset password
+                cout<<"User Id";
+                cin>>id;
+                cout<<"phone number";
+                cin>>ph;
+                l.resetPassword(id,ph);
+                break;
+            case 4:
+                cout<<"User Id";
+                cin>>id;
+                cout<<"Password";
+                cin>>ps;
+                l.changePassword(id,ps);
+                break;
+            case 5://display all account
+                fin.open("login.txt",ios::in | ios::binary);
                 fin.seekg(0,ios::beg);
                 fin.read((char*)&l, sizeof(l));
                 while(!fin.eof()){
-                l.display();
-                fin.read((char*)&l, sizeof(l));
+                    l.display();
+                    fin.read((char*)&l, sizeof(l));
                 }
                 fin.close();
                 getch();
@@ -61,13 +100,19 @@ int main(){
     }    
     return(0);
 }
+void login :: writetofile(){//check garna baaki xa hae
+    fstream fout;
+    fout.open("login.txt",ios::out | ios::app | ios::binary);
+    fout.write((char*)this,sizeof(*this));
+    fout.close();
+}
 void login :: signin(){//to get username and password
     login l;//object of class login
     int count = 0;
     fstream fin;
     userId.clear();
     password.clear();
-    fin.open("login.txt",ios::in);
+    fin.open("login.txt",ios::in | ios::binary);
     cout<<"value of count: "<<count<<endl;
     cout<<endl<<"User ID: ";
     ch = getch();
@@ -118,109 +163,110 @@ void login :: signin(){//to get username and password
     }
 }
 void login :: signup(){// to create new user
+int count=0,check=0;
     fname.clear();
-    lname.clear();
-    cout<<"First Name: ";
-    ch=getch();
-    while(ch != 13){
-        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
-            cout<<ch;
-            fname.push_back(ch);
-            ch=getch();
-        }
-        else if(ch == 8){
-            fname.pop_back();
-            cout<<"\b";
-            ch=getch();
-        }
-    }
-    cout<<endl<<"Last Name: ";
-    ch=getch();
-    while(ch != 13){
-        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
-            cout<<ch;
-            lname.push_back(ch);
-            ch=getch();
-        }
-        else if(ch == 8){
-            lname.pop_back();
-            cout<<"\b";
-            ch=getch();
+    cout<<"First Name: ";{
+        ch=getch();
+        while(ch != 13){
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+                cout<<ch;
+                fname.push_back(ch);
+                ch=getch();
+            }
+            else if(ch == 8){
+                fname.pop_back();
+                cout<<"\b";
+                ch=getch();
+            }
         }
     }
-    cout<<endl<<"Address: ";
-    cin>>address;
-    cout<<"Email ";
-    cin>>email;
-    cout<<"phone no.";
-    cin>>phone;
-    userId="1234";
-    password="happy1234";
     
-}
-void login :: resetPassword(){//check garna baaki xa hae
-    login l;
-    fstream fin, fout;
-    fin.open("login.txt", ios::in);
-    fin.seekg(0,ios::beg);
-    fout.open("login2.txt", ios::app | ios::out);
-    cout<<endl<<"User ID: ";
-    ch = getch();
-    while(ch != 13){
-        if(ch != 8){
+    lname.clear();
+    cout<<endl<<"Last Name: ";{
+        ch=getch();
+        while(ch != 13){
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+                cout<<ch;
+                lname.push_back(ch);
+                ch=getch();
+            }
+            else if(ch == 8){
+                lname.pop_back();
+                cout<<"\b";
+                ch=getch();
+            }
+        }
+    }
+    
+    cout<<endl<<"Address: ";{
+        cin.ignore();
+        getline(cin,address);
+    }
+
+    cout<<endl<<"Email: ";
+
+    cout<<endl<<"Phone No.: ";
+    count =0;
+    while(count <10){
+        ch=getch();
+        if(count==1 && ch!=9){
+            check++;
+            phone.push_back(ch);
             cout<<ch;
-            userId.push_back(ch);
-            ch = getch();
         }
-        else if(ch == 8){
-            cout<<"\b";
-            userId.pop_back();
-            ch = getch();
-        }
-    }
-    cout<<"First Name: ";
-    ch=getch();
-    while(ch != '\n' && ch != ' '){
-        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+        else if(count==2 && (ch!=8 || ch!=7)){
+            check++;
+            phone.push_back(ch);
             cout<<ch;
-            fname.push_back(ch);
-            ch=getch();
         }
-        else if(ch == 8){
-            fname.pop_back();
+        else if (ch == 8){
+            count--;
             cout<<"\b";
-            ch=getch();
+            phone.pop_back();
         }
-    }
-    cout<<"Last Name: ";
-    ch=getch();
-    while(ch != '\n' && ch != ' '){
-        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+        else{
+            count++;
+            phone.push_back(ch);
             cout<<ch;
-            lname.push_back(ch);
-            ch=getch();
-        }
-        else if(ch == 8){
-            lname.pop_back();
-            cout<<"\b";
-            ch=getch();
         }
     }
-    cout<<"phone:";
-    cin>>phone;
-    fin.read((char*)&l,sizeof(l));
-    while(!fin.eof()){
-        if(fname == l.fname && userId == l.userId && phone == l.phone){
-            l.password = "happy123";
-        }
-        fout.write((char*)&l,sizeof(l));
-        fin.read((char*)&l,sizeof(l));
-    }
-    fin.close();
-    fout.close();
-    remove("login.txt");
-    rename("login2.txt","login.txt");
+    
+    cout<<"User ID: ";
+    cin>>userId;
+
     cout<<"Password: ";
+    cin.ignore();
+    getline(cin,password);
+}
+void login :: resetPassword(char* id , char* ph){
+    int count = 0;
+    fstream file;
+    file.open("login.txt",ios::in | ios::out | ios::ate | ios::binary);
+    file.seekg(0,ios::beg);
+    file.read((char*)this, sizeof(*this));
+    while(!file.eof()){
+        if(id == userId && ph == phone){
+            count = 1;
+            getpassword("reset");
+            file.seekp(file.tellp() - sizeof(*this));
+            file.write((char*)this, sizeof(*this));
+        }
+        else if(id == userId && ph != phone){
+            count =2;
+        }
+        file.read((char*)this, sizeof(*this));
+    }
+    file.close();
+    if(count == 0){
+        cout<<"User id not found";
+    }
+    else if(count == 1){
+        cout<<"Password reset to : happy123";
+    }
+    else if(count == 2){
+        cout<<"invalid input";
+    }
+    getch();
 }
 void login :: display(){
     cout<<endl
@@ -232,3 +278,34 @@ void login :: display(){
     <<"Email: "<<email<<endl
     <<"Phone: "<<phone<<endl;
 }
+void login :: changePassword(char* id , char* ps){
+    int count = 0;
+    fstream file;
+    file.open("login.txt",ios::in | ios::out | ios::ate | ios::binary);
+    file.seekg(0,ios::beg);
+    file.read((char*)this, sizeof(*this));
+    while(!file.eof()){
+        if(id == userId && ps == password){
+            count = 1;
+            getpassword("change");
+            file.seekp(file.tellp() - sizeof(*this));
+            file.write((char*)this, sizeof(*this));
+        }
+        else if(id == userId && ps != password){
+            count =2;
+        }
+        file.read((char*)this, sizeof(*this));
+    }
+    file.close();
+    if(count == 0){
+        cout<<"User id not Found";
+    }
+    else if(count == 1){
+        cout<<"Password Changed Successfully";
+    }
+    else if(count == 2){
+        cout<<"Invalid Input";
+    }
+    getch();
+}
+
